299173d0d6118046e5ea23a9862b45f6fff4e2aa
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-02-03 15:53:08 -0300

    understanding NodeJS behind de scenes

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.body);
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body>');
+    res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form>');
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, (error) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    })
+  }
+
   res.setHeader('Content-Type', 'text/html');
-  res.write('<html><head><title>My first page</title></head><body><h1>Faaala galera</h1></body></html>');
-  res.end();
+  res.write('<html>');
+  res.write('<head><title>My first page</title></head>');
+  res.write('<body>');
+  res.write('<h1>Faaala galera</h1>')
+  res.write('</body>');
+  res.write('</html>');
+  return res.end();
+  console.log(req.url, req.method, req.body);
+
 })
 
 server.listen(3000);
