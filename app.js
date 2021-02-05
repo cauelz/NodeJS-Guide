18dc4923a158cc965e66ee8a8e6e6f696bbd6553
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-02-05 16:42:10 -0300

    template engines ejs

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -13,8 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404);
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
