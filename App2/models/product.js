1dc4266a0500975ea9518a83597fcd1e9242fcd4
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-02-06 18:10:28 -0300

    Saving data into a json file

--- App2/models/product.js
+++ App2/models/product.js
@@ -1,16 +1,43 @@
-const products = [];
-
-class Product {
+const fs = require('fs');
+const path = require('path');
+module.exports = class Product {
 
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      'data',
+      'products.json'
+    );
+    console.log(require.main.filename);
+    fs.readFile(p, (error, data) => {
+      let products = [];
+
+      if (!error) {
+        products = JSON.parse(data);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (error) => {
+        console.log(error);
+      });
+    });
   }
 
   static fetchAll() {
-    return this.products;
+    const p = path.join(
+      path.dirname(require.main.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (error, data) => {
+      if (error) {
+        return [];
+      }
+      return JSON.parse(data);
+    })
+    return products;
   }
 }
