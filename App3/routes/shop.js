2864a0ff154e726a837a4ebbb9fae0cb9ad63e34
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-02-09 08:20:57 -0300

    dynamics routes created with the product id

--- App3/routes/shop.js
+++ App3/routes/shop.js
@@ -7,13 +7,10 @@ const shopController = require('../controllers/shop');
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
-
 router.get('/products', shopController.getProducts);
-
+router.get('/products/:productId', shopController.getProducts);
 router.get('/cart', shopController.getCart);
-
 router.get('/orders', shopController.getOrders);
-
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
